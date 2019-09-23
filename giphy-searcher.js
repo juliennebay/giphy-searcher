8967cd2a4817d39a1f5075df075da1831ba2cf76
Julie <julienne.bay@gmail.com>
2019-09-23 19:00:54 -0400

    style the app - add margins

--- giphy-searcher.js
+++ giphy-searcher.js
@@ -4,6 +4,8 @@ function loadScript(){
     const button = document.querySelector("button")
     const ul = document.querySelector("ul")
     const input = document.querySelector("input")
+    const loading = document.querySelector(".loading")
+    loading.hidden = true;
 
     function searchGifs(){
         //clear old results, if any
@@ -11,14 +13,20 @@ function loadScript(){
 
         const queryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${input.value}&limit=2`
 
+        loading.hidden = false
+        button.disabled = true
+
         fetch(queryUrl).then(response => response.json())
                        .then(result => {
+                            loading.hidden = true
+                            button.disabled = false
                            const gifs = result.data //data is an array
                            gifs.forEach(gif => {
                             const li = document.createElement("li")
                             const img = document.createElement("img")
                             img.src = gif.images.downsized_medium.url
                             li.appendChild(img)
+                            li.classList.add("listItem")
                          //   li.textContent = "gif slug :" + gif.slug
                             ul.appendChild(li)
                            })
